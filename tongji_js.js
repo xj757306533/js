e923d99e502256b03683e1e7913a0fb7f8ecdad0
gin <18624323263@163.com>
2018-05-23 13:51:16 +0800

    baidutuisong2

---
+++ tongji_js.js
@@ -0,0 +1,17 @@
+/**
+ * Created by gin on 2018/5/23.
+ */
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?20c56b40ac074ded3f1803ba2d1ae039";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?86f3435f594158b2dd6334fc6d14b0e2";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
